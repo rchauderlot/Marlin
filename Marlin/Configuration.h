c52deeb5244da84ea4780d38ef276d07ca4c3052
Roberto Chauderlot Marchamalo <rchauderlot@gmail.com>
2019-01-11 20:34:49 +0100

    Configured the bed pid values using a command to autotune them.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -431,9 +431,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  //#define DEFAULT_bedKp 10.00
+  //#define DEFAULT_bedKi .023
+  //#define DEFAULT_bedKd 305.4
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
@@ -442,6 +442,12 @@
   //#define DEFAULT_bedKd 1675.16
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
+
+  // RCH: obtained via "M303 E-1" command
+  #define DEFAULT_bedKp 37.90
+  #define DEFAULT_bedKi 1.39
+  #define DEFAULT_bedKd 257.70
+  
 #endif // PIDTEMPBED
 
 // @section extruder
