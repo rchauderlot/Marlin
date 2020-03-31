7149b6867e2cfb7bca81883feed82061d365c4ac
Roberto Chauderlot Marchamalo <rchauderlot@gmail.com>
2020-03-31 19:35:16 +0200

    Added some configuration changes due to the change of the cables of the motors for xh254 connectors.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1905,7 +1905,7 @@
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       900        // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT      1000        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS    256    // 0..256
     #define X_RSENSE          0.11
@@ -1921,7 +1921,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       900
+    #define Y_CURRENT      1000
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS    256
     #define Y_RSENSE          0.11
